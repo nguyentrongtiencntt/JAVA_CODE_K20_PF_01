7d5d5e3e67eaa0c1f0cfcd133a498f8166f4ad41
nguyentrongtiencntt <nguyentrongtiencntt@gmail.com>
2021-05-10 10:47:46 +0700

    add thread

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,18 @@
 public class Main {
     public static void main(String[] args) {
-        System.out.println("Function 01");
+
+        //Thread 1
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println("a");
+        }
+        
+        //Thread 2
+        for (int i = 0; i < 10 ; i++) {
+            System.out.println("a");
+        }
+
+
+        System.out.println("main end!");
 
     }
 }
